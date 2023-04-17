301ddc4d0ac41dd8cdbe6e9b150b5919b3154b8a
Christopher Cruz <78170426+chriscr07@users.noreply.github.com>
2023-04-17 20:46:56 +0000

    pop-up btn "ver" productos, css y js Bootstrap

--- catalogoProductos.php
+++ catalogoProductos.php
@@ -19,7 +19,8 @@
                         <h5>Precio: $$$$$$$$</h5>
                         <br>
                         <div class="botones">
-                            <a href="#" class="btn btn-warning"><i class="fa-regular fa-eye"></i> Ver</a>
+                        <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-eye"></i> Ver</a>
+
                             <a href="#" class="btn btn-warning"><i class='far fa-plus-square'></i> Añadir</a>
                         </div>
                     </div>
@@ -34,7 +35,7 @@
                         <h5>Precio: $$$$$$$$</h5>
                         <br>
                         <div class="botones">
-                            <a href="#" class="btn btn-warning"><i class="fa-regular fa-eye"></i> Ver</a>
+                        <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-eye"></i> Ver</a>
                             <a href="#" class="btn btn-warning"><i class='far fa-plus-square'></i> Añadir</a>
                         </div>
                     </div>
@@ -49,7 +50,7 @@
                         <h5>Precio: $$$$$$$$</h5>
                         <br>
                         <div class="botones">
-                            <a href="#" class="btn btn-warning"><i class="fa-regular fa-eye"></i> Ver</a>
+                        <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-eye"></i> Ver</a>
                             <a href="#" class="btn btn-warning"><i class='far fa-plus-square'></i> Añadir</a>
                         </div>
                     </div>
@@ -64,7 +65,7 @@
                         <h5>Precio: $$$$$$$$</h5>
                         <br>
                         <div class="botones">
-                            <a href="#" class="btn btn-warning"><i class="fa-regular fa-eye"></i> Ver</a>
+                        <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-eye"></i> Ver</a>
                             <a href="#" class="btn btn-warning"><i class='far fa-plus-square'></i> Añadir</a>
                         </div>
                     </div>
@@ -79,7 +80,7 @@
                         <h5>Precio: $$$$$$$$</h5>
                         <br>
                         <div class="botones">
-                            <a href="#" class="btn btn-warning"><i class="fa-regular fa-eye"></i> Ver</a>
+                        <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-eye"></i> Ver</a>
                             <a href="#" class="btn btn-warning"><i class='far fa-plus-square'></i> Añadir</a>
                         </div>
                     </div>
@@ -94,11 +95,31 @@
                         <h5>Precio: $$$$$$$$</h5>
                         <br>
                         <div class="botones">
-                            <a href="#" class="btn btn-warning"><i class="fa-regular fa-eye"></i> Ver</a>
+                        <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-regular fa-eye"></i> Ver</a>
                             <a href="#" class="btn btn-warning"><i class='far fa-plus-square'></i> Añadir</a>
                         </div>
                     </div>
                 </div>
+
+                <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Titulo del producto</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <img src="img/productotest.jpg" class="img-fluid" preload>
+        <p>Some quick example text to build on the card title and make up the bulk
+        of the card's content.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
                 <a href="productos.php">Listado de productos para edicion</a>
 
             </div>
