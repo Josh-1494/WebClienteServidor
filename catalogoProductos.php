0ed8294b9211d8a9050c94ae6bd96ea2873f648b
Josh-1494 <79804411+Josh-1494@users.noreply.github.com>
2023-04-18 11:29:05 -0600

    Centrado del card de ver listo

--- catalogoProductos.php
+++ catalogoProductos.php
@@ -92,23 +92,26 @@
                 </div>
 
                 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Titulo del producto</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <img src="img/productotest.jpg" class="img-fluid" preload>
-        <p>Some quick example text to build on the card title and make up the bulk
-        of the card's content.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-      </div>
-    </div>
-  </div>
-</div>
+            <div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Titulo del producto</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <img src="img/productotest.jpg" class="img-fluid" preload>
+                        <p>Some quick example text to build on the card title and make up the bulk
+                        of the card's content.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+               
 
                 <!-- <a href="productos.php">Listado de productos para edicion</a> -->
